f8d006155a657f74cb79d143e45bd58078b741de
fathy <fathy@DESKTOP-KL6HPHJ>
2018-10-21 15:05:59 +0200

    insert and retrieve user with repeated process in 10 sec

---
+++ CURDDerbyQuartz/src/main/java/com/quartz/demo/service/QuertzBuilder.java
@@ -0,0 +1,34 @@
+package com.quartz.demo.service;
+
+import org.apache.log4j.Logger;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class QuertzBuilder {
+	private static Logger logger= Logger.getLogger(QuertzBuilder.class);
+	
+	public void buildQuartz() {
+		try {
+			JobDetail userJobDetails=JobBuilder.newJob(UserQuartzJob.class).
+					withIdentity("user","employees")
+					.build();
+			Trigger userTrigger=TriggerBuilder.newTrigger().withIdentity("cronTrigger1", "group1")
+								.withSchedule(CronScheduleBuilder.cronSchedule("0 0/2 * * * ?")).build();
+
+			Scheduler userScheduler=new StdSchedulerFactory().getScheduler();
+			userScheduler.start();
+			userScheduler.scheduleJob(userJobDetails, userTrigger);
+		} catch (SchedulerException e) {
+			logger.error(e.toString());
+		}
+		
+		
+	}
+
+}
